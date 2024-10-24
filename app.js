06e439415c127fac7d23cf96cd8b31d523d920ea
ganesh <koreganesh114@gmail.com>
2024-10-24 10:17:56 +0530

    modules installed

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+const heading = React.createElement("h1" , {} , "WElcome to to-do-list");
+
+
+
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,6 @@
+impo;
+const heading = React.createElement("h1", {}, "WElcome to to-do-list");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
